42f4b45331aa761f2cdd09c0b8794732f27219a9
Vishal Mehta <vrmehta1@asu.edu>
2014-04-24 21:13:36 -0700

    Update Token.cpp

--- Token.cpp
+++ Token.cpp
@@ -55,7 +55,11 @@ string Token::getTokenString()
 
 void Token::print()
 {
+    char line[MAX_SOURCE_LINE_LENGTH + 32];
+	const char *symbol_string = SYMBOL_STRINGS[this->getCode()];
 
+	sprintf(line, "    >> %-16s %s\n", symbol_string, this->getTokenString());
+	printf("%s", line);
 }
 
 //
