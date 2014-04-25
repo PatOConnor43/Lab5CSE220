a9ec607e570e7623ba2d9b113129080819bf767d
Vishal Mehta <vrmehta1@asu.edu>
2014-04-24 20:22:22 -0700

    Create Identifier.h

---
+++ Identifier.h
@@ -0,0 +1,34 @@
+/*
+ * Identifier.h
+ *
+ *  Created on: Apr 20, 2014
+ *      Author: Vishal
+ */
+
+#ifndef IDENTIFIER_H_
+#define IDENTIFIER_H_
+
+#include "Token.h"
+
+class Identifier : public Token
+{
+public:
+	Identifier();
+	~Identifier();
+
+    void setLeftChild(Identifier *tok); // OUT
+    Identifier *getLeftChild();
+    void setRightChild(Identifier *tok);
+    Identifier *getRightChild();
+    void addToLineNumberList(LineNumberList *listItem);
+    LineNumberList *getLineNumberList();
+    void print();
+
+private:
+    //What variables and methods am I missing to implement a binary tree.
+    Token *leftChild;
+    Token *rightChild;
+    LineNumberList *list;
+};
+
+#endif /* IDENTIFIER_H_ */
